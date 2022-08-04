d294ce063f1e3cac439dd8408a6e2895de79daf3
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2022-08-04 16:17:34 +0300

    Initial commit

---
+++ src/main/java/PracticeFormPage.java
@@ -0,0 +1,13 @@
+public class PracticeFormPage {
+    public void setName(){
+
+    }
+
+    public void serLastName(){
+
+    }
+
+    public void email(){
+
+    }
+}
---
+++ src/test/java/PersonalInformationTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class PersonalInformationTest {
+
+    @Test
+    public void test_setName(){
+        System.out.println("Test method 1");
+    }
+    @Test
+    public void test_setLastName(){
+        System.out.println("Test method 2");
+
+    }
+}
